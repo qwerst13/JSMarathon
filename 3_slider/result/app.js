c7dc7d7990d10fe559ccd1052a7184dc983391fe
biohazard <qwerst13@gmail.com>
2021-07-11 00:32:27 +0300

    third homework complete

--- 3_slider/result/app.js
+++ 3_slider/result/app.js
@@ -1,33 +1,29 @@
-const upBtn = document.querySelector('.up-button');
-const downBtn = document.querySelector('.down-button');
-const sidebar = document.querySelector('.sidebar');
-const mainSlide = document.querySelector('.main-slide');
-const container = document.querySelector('.container');
-const slidesCount = mainSlide.querySelectorAll('div').length;
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const container = document.querySelector(".container");
+const slidesCount = mainSlide.querySelectorAll("div").length;
 let activeSlideIndex = 0;
 
-sidebar.style.top = `-${(slidesCount-1) * 100}vh`;
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
-upBtn.addEventListener('click', () => {
-    changeSlide('up');
-});
+upBtn.addEventListener("click", () => changeSlide("up"));
 
-downBtn.addEventListener('click', () => {
-    changeSlide('down');
-});
+downBtn.addEventListener("click", () => changeSlide("down"));
 
 function changeSlide(direction) {
-    if(direction === 'up') {
-        activeSlideIndex++;
-        if (activeSlideIndex === slidesCount) activeSlideIndex = 0;
-    } else {
-        activeSlideIndex--;
-        if (activeSlideIndex < 0) activeSlideIndex = slidesCount - 1;
-    }
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) activeSlideIndex = 0;
+  } else {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) activeSlideIndex = slidesCount - 1;
+  }
 
-    const height = container.clientHeight;
+  const height = container.clientHeight;
 
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
 
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
