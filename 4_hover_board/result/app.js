f7e94f5b2d2d66b432e6705ef989d8223243b1a5
biohazard <qwerst13@gmail.com>
2021-07-12 03:53:55 +0300

    fourth homework complete

---
+++ 4_hover_board/result/app.js
@@ -0,0 +1,35 @@
+const board = document.getElementById('board');
+const SQUARES_NUMBER = 400;
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'cyan', 'lime']
+
+for (let i=0; i<SQUARES_NUMBER; i++) {
+  const sqare = document.createElement('div');
+  sqare.classList.add('square');
+
+  sqare.addEventListener('mouseover', () => {
+    setColor(sqare)
+  });
+
+  sqare.addEventListener('mouseleave', () => {
+    removeColor(sqare)
+  });
+
+  board.append(sqare);
+}
+
+function setColor(el) {
+  const color = getRandomColor();
+  el.style.backgroundColor = color;
+  el.style.boxShadow = `0 0 2px ${color}, 0 0 8px ${color}`
+}
+
+function removeColor(el) {
+  el.style.backgroundColor = '#1d1d1d';
+  el.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+
+  return colors[index];
+}
